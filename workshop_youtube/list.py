60f2961327b4187a2b7a92c9e31bfc6bd231e258
George <ponosov.qa@gmail.com>
2025-04-09 13:16:26 +0200

    Поработал с списком

---
+++ workshop_youtube/list.py
@@ -0,0 +1,7 @@
+fruits = ["apple", "banana","cherry"]
+print(fruits)
+
+print(len(fruits)) #Колличесво элиментов в списке
+
+my_list = list(("apple", 1, 1.5, True)) # Для нескольких атриубтов в списке нужно использовать(("первое значение" , 2)) или можно использовать []
+print(my_list)
