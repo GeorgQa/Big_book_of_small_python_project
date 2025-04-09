0ac48fc521572e2c80952d126ba419b4ecfcb244
George <ponosov.qa@gmail.com>
2025-04-09 11:27:41 +0200

    Вернул обратно блок __name__ == "__main__":

--- workshop_youtube/conditional_operators.py
+++ workshop_youtube/conditional_operators.py
@@ -1,6 +1,35 @@
-if True:
-    print("Чет там")
+# if True:
+#     print("Чет там")
+#
+# #Все что внутри if выполняеется только есть условие == True
+# if False:
+#     print("Где-то там")
+from gemes.bitMapMessage import message
 
-#Все что внутри if выполняеется только есть условие == True
-if False:
-    print("Где-то там")
+# x = 1
+# #если увдолетворяет сразу 2 условиям то пойдет в перовое
+# if x > 0:
+#     print("x is positive")
+# elif x < 0:
+#     print("x is neative")
+# else:
+#     print("x is zero")
+
+
+# x = 10
+# y = 20
+# if  x > 0:
+#     if y > 0:
+#         print("x and y are postive")
+# ниже приведен пример он более правильный
+
+x = -1
+y = 20
+if  x > 0 and y > 0:
+        print("x and y are postive")
+else:
+    print("x or y negative")
+
+text = "textvrfergf"
+if text:
+    print("massage is not empty")
